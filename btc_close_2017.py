fa3a914baba859a39d5674ecc5fcba775730f98f
zxy <937919473@qq.com>
2018-08-13 17:32:00 +0800

    收盘价月，周，星期均值

--- btc_close_2017.py
+++ btc_close_2017.py
@@ -1,15 +1,16 @@
 
-# import requests
-# json_url = 'https://raw.githubusercontent.com/muxuezi/btc/master/btc_close_2017.json'
+import requests
+json_url = 'https://raw.githubusercontent.com/muxuezi/btc/master/btc_close_2017.json'
 
-# req=requests.get(json_url)
-# # 将数据写入文件
-# with open('btc_close_2017_request.json', 'w') as f: 
-#     f.write(req.text)
-# file_requests=req.json()
+req=requests.get(json_url)
+# 将数据写入文件
+with open('btc_close_2017_request.json', 'w') as f: 
+    f.write(req.text)
+file_requests=req.json()
 import json 
 import pygal
 import math
+from itertools import groupby
 
 filename = 'btc_close_2017.json'
 with open(filename) as f:
@@ -41,30 +42,44 @@ for btc_dict in btc_data:
 	close.append(int(float(btc_dict['close'])))
 
 
-line_chart=pygal.Line(x_label_rotation=20,show_minor_x_labels=False)
-line_chart.title='收盘价对数变换(￥)'
-line_chart.x_labels=dates
-N=20#x坐标轴每隔20天显示一次
-line_chart.x_labels_major=dates[::N]
-close_log=[math.log10(_) for _ in close]
-line_chart.add('log收盘价',close_log)
-line_chart.render_to_file('收盘折对数变换价线图(￥).svg')
+# line_chart=pygal.Line(x_label_rotation=20,show_minor_x_labels=False)
+# line_chart.title='收盘价对数变换(￥)'
+# line_chart.x_labels=dates
+# N=20#x坐标轴每隔20天显示一次
+# line_chart.x_labels_major=dates[::N]
+# close_log=[math.log10(_) for _ in close]
+# line_chart.add('log收盘价',close_log)
+# line_chart.render_to_file('收盘折对数变换价线图(￥).svg')
 
-from itertools import groupby
 
-def draw_line(x_date,y_date,title,y_legend):
-    xy_map=[]
-    for x,y in groupby(sorted(zip(x_date,y_date)),key=lambda _:_[0]):
-        y_list=[v for _,v in y]
-        xy_map.append([x,sum(y_list)/len(y_list)])
-        x_unique,y_mean=[*zip(*xy_map)]
-        line_chart=pygal.Line()
-        line_chart.title=title
-        line_chart.x_labels=x_unique
-        line_chart.add(y_legend,y_mean)
-        line_chart.render_to_file(title+'.svg')
-        return line_chart
 
-        idx_month=dates.index('2017-12-01')
-        line_chart_month=draw_line(months[:idx_month],close[:idx_month],'收盘价月日均值（￥）','月日均值')
-        line_chart_month
+def draw_line(x_data, y_data, title, y_legend):
+    xy_map = []
+    for x, y in groupby(sorted(zip(x_data, y_data)), key=lambda _: _[0]):  
+        y_list = [v for _, v in y]
+        xy_map.append([x, sum(y_list) / len(y_list)])  
+    x_unique, y_mean = [*zip(*xy_map)]  
+    line_chart = pygal.Line()
+    line_chart.title = title
+    line_chart.x_labels = x_unique
+    line_chart.add(y_legend, y_mean)
+    line_chart.render_to_file(title + '.svg')
+    return line_chart
+
+#收盘价月日均值
+idx_month=dates.index('2017-12-01')
+line_chart_month=draw_line(months[:idx_month],close[:idx_month],'收盘价月日均值（￥）','月日均值')
+line_chart_month
+
+#收盘价周日均值
+idx_week=dates.index('2017-12-11')
+line_chart_week=draw_line(weeks[1:idx_week],close[1:idx_week],'收盘价周日均值（￥）','周日均值')
+line_chart_week
+
+#收盘价星期均值
+idx_week=dates.index('2017-12-11')
+wd=['Monday','Tuesday','Wednesday','Thursday','Friday','Saturday','Sunday']
+weekdays_int=[wd.index(w)+ 1 for w in weekdays[1:idx_week]]
+line_chart_weekday=draw_line(weekdays_int,close[1:idx_week],'收盘价星期均值(￥)','星期均值')
+line_chart_weekday.x_labels=['周一','周二','周三','周四','周五','周六','周日']
+line_chart_weekday.render_to_file('收盘价星期均值（￥）.svg')
